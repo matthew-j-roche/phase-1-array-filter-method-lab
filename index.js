9a06e1d136ff3a5652af92328a66faf13da301a3
matt roche <matt.roche@gmail.com>
2023-03-06 19:49:24 -0600

    complete array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+  }
+
+  findMatching(drivers, 'Bobby');
+
+  findMatching(drivers, 'Sammy');
+  
+  findMatching(drivers, 'Susan');
+
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+  
+fuzzyMatch(drivers, 'Sa');
+
+fuzzyMatch(drivers, 'y');
+
+fuzzyMatch(drivers, 'mm');
+
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
